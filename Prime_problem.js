c01bc791506c70f1f7fdda804e2feba0cc8b2b7f
Aanchal baidya <baidyaaanchal@gmail.com>
2022-09-14 13:58:04 +0530

    first promble

--- Prime_problem.js
+++ Prime_problem.js
@@ -1,13 +1,13 @@
  let n=21;
  let prime="true";
  if(n==1){
-    console.log('No');
+    console.log('Yes');
  }  else { 
- for(i=2;i<=n;j++){
+ for(i=2;i<=n;i++){
     if(n%i==0){
         prime="false";
         break;
     }
 }
- prime==false?console.log("Yes"):console.log("No");
+ prime==false?console.log("false"):console.log("Yes");
  }
