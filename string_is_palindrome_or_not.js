dee8144dd7f13311a017826b50f2c4dc688cc610
Aanchal baidya <baidyaaanchal@gmail.com>
2022-09-14 18:10:04 +0530

    help_me_error_not_found

--- string_is_palindrome_or_not.js
+++ string_is_palindrome_or_not.js
@@ -1,8 +1,8 @@
 
-let str=['ujjwal']
+let str=['naman']
 
 let strcopy="";
-for(i=str.length;i>=0;i--){
+for(i=str.length;i<0;i--){
     strcopy=str;
 } 
 str==strcopy?console.log("Palindrome"):console.log("Not Palindrome");
