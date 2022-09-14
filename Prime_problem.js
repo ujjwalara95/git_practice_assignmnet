9f4da6cdefcd191cf406d0de1140286063017c99
Aanchal baidya <baidyaaanchal@gmail.com>
2022-09-14 23:07:37 +0530

    everything fine

--- Prime_problem.js
+++ Prime_problem.js
@@ -1,15 +1,13 @@
-
-let n =21;
-
-let prime=true;
-if(n==1){
-  console.log("yes");
-}else{
-  for(i=2;i<=n;i++){
-    if(n%2==0){
-      prime=false;
-      break;
+let n=13;
+ let prime=true;
+ if(n==1){
+    console.log('No');
+ }  else { 
+ for(i=2;i<n;i++){
+    if(n%i===0){
+        prime=false;
+        break;
     }
-  }
 }
-prime==false?console.log("No") : console.log("Yes");
+ prime===true?console.log("Yes"):console.log("No");
+ }
